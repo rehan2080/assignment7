645fd6e7c8eeadf345215aba43ac944dc3b14e70
rehan2080 <rehanaslam2080@gmail.com>
2020-07-04 14:39:39 +0500

    Assingment no 7 Calculator

---
+++ app.js
@@ -0,0 +1,32 @@
+
+
+function calc(num)
+{
+
+a = document.getElementById("name");
+
+a.value += num;
+
+}
+
+
+
+
+function clear1()
+{
+
+a = document.getElementById("name");
+
+a.value = "";
+}
+
+
+function result()
+{
+
+a = document.getElementById("name");
+
+a.value = eval(a.value)
+}
+
+
